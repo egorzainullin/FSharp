858780ab910e1b36c093e23d6b3a14c14e6edb3b
egorzainullin <egor.zainullin@outlook.com>
2018-04-01 20:59:32 +0300

    added task 2

---
+++ HomeWork5/PointFree/PointFree/Program.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+namespace Hw5
+
+module PointFree =
+
+    let f1 x l = List.map ((*) x) l
+
+    let f2 x = List.map ((*) x)
+    
+    let f3 = List.map << (*)
+    
+    do f3 2 [1; 2; 3] |> printfn "%A" 
+
+    [<EntryPoint>]
+    let main argv = 
+        printfn "%A" argv
+        0 // return an integer exit code
