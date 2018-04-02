c3c4bdaa8324e1611cf904f11323433c642e6a4d
egorzainullin <egor.zainullin@outlook.com>
2018-04-02 19:52:22 +0300

    tests added

---
+++ HomeWork4/Interpreter/InterpreteTests/InterpreteTests.fs
@@ -0,0 +1,36 @@
+﻿module InterpreterTests
+
+open NUnit.Framework
+open FsUnit
+open Interprete
+
+[<Test>]
+let ``when should not change``() =
+    let term = Application(Variable("x"), Variable("y"))
+    let transformed = betaTransform term
+    transformed |> should equal term
+
+[<Test>]
+let ``IdId is id``() =
+    let term = Lambda("x", Application(Lambda("x", Variable("x")), Variable("x")))
+    let transformed = betaTransform term
+    transformed |> should equal (Lambda("x", Variable("x")))
+
+[<Test>]
+let ``K id term is id``() = 
+    let term = Application(Application(Lambda("x", Lambda("y", Variable("x"))), Lambda("z", Variable("z"))), Variable("m"))
+    term |> betaTransform |> betaTransform |> betaTransform |> should equal (Lambda("z", Variable("z")))
+
+[<Test>]
+let ``test basic substitute``() =
+    let substitution = Variable("y")
+    let term1 = Variable("x")
+    (substitute "x" substitution term1) |> should equal (Variable("y"))
+    let term2 = Lambda("x", Variable("x"))
+    (substitute "x" substitution term2) |> should equal term2
+
+[<Test>]
+let ``test complex substitute``() = 
+    let substitution = Variable("z")
+    let term = Lambda("y", Application(Variable("x"), Variable("y")))
+    (substitute "x" substitution term) |> should equal (Lambda("y", Application(Variable("z"), Variable("y"))))
--- HomeWork4/Interpreter/Interpreter/Program.fs
+++ HomeWork4/Interpreter/Interpreter/Program.fs
@@ -8,16 +8,6 @@ type Term =
     | Application of Term * Term
     | Lambda of string * Term
 
-let rec getStringToPrint term = 
-    match term with
-    | Variable(x) -> x
-    | Application(t1, t2) -> "(" + getStringToPrint t1 + ")" + "(" + getStringToPrint t2 + ")"  
-    | Lambda(x, term) -> "(" + "lambda " + x + "." + getStringToPrint term + ")"
-
-let printToConsole term = getStringToPrint term |> printfn "%s"
-
-let example = Application(Lambda("x", Application(Variable("x"), Variable("y"))), Variable("z"))
-
 let rec substitute variable substitution term =
     match term with 
     | Variable(x) -> if (x = variable) then substitution else Variable(x)
@@ -25,30 +15,20 @@ let rec substitute variable substitution term =
     | Lambda(var, term) -> if (var <> variable) then Lambda(var, substitute variable substitution term)
                                                 else Lambda(var, term)
 
-do printToConsole example
-
 let betaTransform term =
     let rec betaTransform' term = 
         match term with
         | Application(Lambda(var, term) , t2) -> (substitute var t2 term, true)
         | Application(t1, t2) -> 
                                  let (beta1, isChanged1) = betaTransform' t1
-                                 if not isChanged1 then (Application(beta1, t2), true)
+                                 if isChanged1 then (Application(beta1, t2), true)
                                  else let (beta2, isChanged2) = betaTransform' t2 in 
-                                        if not isChanged2 then (Application(t1, beta2), true)
+                                        if isChanged2 then (Application(t1, beta2), true)
                                         else (Application(t1, t2), false) 
         | Lambda(var, term) -> let (transformed, isChanged) = betaTransform' term
                                (Lambda(var, transformed), isChanged)
         | Variable(x) -> (Variable(x), false)
     betaTransform' term |> fst
-
-let term1 = Application(Lambda("y", Lambda("x", Application(Variable("x"), Variable("y")))), Variable("d"))
-
-let idId = Lambda("x", Application(Lambda("x", Variable("x")), Variable("x"))) 
-
-do printToConsole idId
-
-do betaTransform idId |> printToConsole
 
 [<EntryPoint>]
 let main argv = 
