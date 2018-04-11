fa0384b764fbd6a20f1c06f0270434fba5e62ff5
egorzainullin <egor.zainullin@outlook.com>
2018-04-11 12:15:34 +0300

    tests added

---
+++ HomeWork5/PointFree/Hw5Tests/Tests.fs
@@ -0,0 +1,24 @@
+﻿module Hw5Test
+
+open FsCheck
+open Hw5
+open Hw5.PointFree
+open NUnit.Framework
+open FsUnit
+
+let f x l = List.map (fun y -> y * x) l
+
+[<Test>]
+let ``check equality 1``() = 
+    let func x l = (f x l) = (f1 x l)
+    Check.QuickThrowOnFailure func
+
+[<Test>]
+let ``check equality 2``() = 
+    let func x l = (f x l) = (f2 x l)
+    Check.QuickThrowOnFailure func
+
+[<Test>]
+let ``check equality 3``() = 
+    let func x l = (f x l) = (f3 x l)
+    Check.QuickThrowOnFailure func
