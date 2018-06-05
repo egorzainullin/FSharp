e1b5c1e42219e9cee9369267b8058c4a566dcfa2
egorzainullin <egor.zainullin@outlook.com>
2018-06-05 20:24:16 +0300

    -fixed : deleted some invented bikes

--- HomeWork7/HomeWork7/HomeWork7/Program.fs
+++ HomeWork7/HomeWork7/HomeWork7/Program.fs
@@ -1,21 +1,9 @@
 ﻿open System
 
 let convertToInt (str : string) =
-    let rec convertToInt' (str : string) result i =
-        if str.Length = i then Some(result)
-        else match str.[i] with
-             | '0' -> convertToInt' str (result * 10) (i + 1)
-             | '1' -> convertToInt' str (result * 10 + 1) (i + 1)
-             | '2' -> convertToInt' str (result * 10 + 2) (i + 1)
-             | '3' -> convertToInt' str (result * 10 + 3) (i + 1)
-             | '4' -> convertToInt' str (result * 10 + 4) (i + 1)
-             | '5' -> convertToInt' str (result * 10 + 5) (i + 1)
-             | '6' -> convertToInt' str (result * 10 + 6) (i + 1)
-             | '7' -> convertToInt' str (result * 10 + 7) (i + 1)
-             | '8' -> convertToInt' str (result * 10 + 8) (i + 1)
-             | '9' -> convertToInt' str (result * 10 + 9) (i + 1)
-             | _ -> None
-    convertToInt' str 0 0
+    match Int32.TryParse str with
+    | true, num -> Some(num)
+    | _ -> None
 
 type StringConvertBuilder() =
      member this.Bind(x, f) =
@@ -25,9 +13,8 @@ type StringConvertBuilder() =
         | None -> None
      member this.Return(x) = Some(x)    
 
-let convertToExactPrecise x numberOfDigits = 
-    let z = (float) (pown 10 numberOfDigits)
-    x * z |> round |> (/) <| z
+let convertToExactPrecise (x : float) (numberOfDigits : int) = 
+    Math.Round(x, numberOfDigits)
 
 type PreciseBuilder(precise) = 
      new() = PreciseBuilder(0)
